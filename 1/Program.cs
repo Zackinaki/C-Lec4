7406c65aef642798f776a3b86e00d3189af36982
IrinchinovZh <exp.bur@gmail.com>
2022-12-07 17:16:01 +0800

    add

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿string[,]table=new string[2,5];
+table[1,2]="slovo";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows,columns]}-");
+    }
+}
+
+
